c16c629be82fbf6c3925b25f74406efe264a98ec
namnt-fptu <namntse170239@fpt.edu.vn>
2024-09-18 09:19:45 +0700

    Refactor KoiFishAPI

---
+++ src/main/java/com/group10/koiauction/exception/EntityNotFoundException.java
@@ -0,0 +1,7 @@
+package com.group10.koiauction.exception;
+
+public class EntityNotFoundException extends RuntimeException {
+    public EntityNotFoundException(String message) {
+        super(message);
+    }
+}
