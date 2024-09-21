b6a593c3e54c8d1c91bc19fa32d095a486778e79
namnt-fptu <namntse170239@fpt.edu.vn>
2024-09-21 08:32:12 +0700

    Set up Spring Security : password encoder , authentication

---
+++ src/main/java/com/group10/koiauction/model/response/AccountResponse.java
@@ -0,0 +1,18 @@
+package com.group10.koiauction.model.response;
+
+import com.group10.koiauction.entity.enums.AccountRoleEnum;
+import com.group10.koiauction.entity.enums.AccountStatusEnum;
+import lombok.Data;
+
+@Data
+public class AccountResponse {
+    private long user_id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
+    private String address;
+    private AccountStatusEnum status;
+    private AccountRoleEnum roleEnum;
+
+}
