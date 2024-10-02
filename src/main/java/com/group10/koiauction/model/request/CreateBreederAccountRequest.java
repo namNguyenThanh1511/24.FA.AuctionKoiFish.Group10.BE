5ef8ce617348ce8ede110232571e3162fa1f7761
khaqng <khanhquangnguyennqk@gmail.com>
2024-10-02 09:32:49 +0700

    add create breeder account

---
+++ src/main/java/com/group10/koiauction/model/request/CreateBreederAccountRequest.java
@@ -0,0 +1,42 @@
+package com.group10.koiauction.model.request;
+
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class CreateBreederAccountRequest {
+    @NotBlank(message = "Username is required")
+    @Size(min = 3, max = 50, message = "Username must be between 3 and 50 characters")
+    private String username;
+
+    @NotBlank(message = "First name is required")
+    @Size(min = 2, max = 50, message = "First name must be between 2 and 50 characters")
+    private String firstName;
+
+    @NotBlank(message = "Last name is required")
+    @Size(min = 2, max = 50, message = "Last name must be between 2 and 50 characters")
+    private String lastName;
+
+    @Email(message = "Please provide a valid email address")
+    @NotBlank(message = "Email is required")
+    @Column(unique = true)
+    private String email;
+
+    @NotBlank(message = "Phone number is required")
+    @Pattern(regexp = "(84|0[3|5|7|8|9])+(\\d{8})\\b", message = "Phone number is invalid")
+    @Column(unique = true)
+    private String phoneNumber;
+
+
+    @NotBlank(message = "Address is required")
+    @Size(max = 255, message = "Address cannot exceed 255 characters")
+    private String address;
+}
