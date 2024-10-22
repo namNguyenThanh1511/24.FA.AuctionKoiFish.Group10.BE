c49f8e4a0d33a176628c095d46046b44fe93c0de
khaqng <khanhquangnguyennqk@gmail.com>
2024-10-22 18:05:47 +0700

    api get list transaction of current account

--- src/main/java/com/group10/koiauction/api/TransactionAPI.java
+++ src/main/java/com/group10/koiauction/api/TransactionAPI.java
@@ -6,10 +6,7 @@ import com.group10.koiauction.service.TransactionService;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -28,4 +25,11 @@ public class TransactionAPI {
         List<TransactionResponseDTO> transactions = transactionService.getAllTransactions();
         return ResponseEntity.ok(transactions);
     }
+
+    @GetMapping("/transactions")
+    public ResponseEntity<List<TransactionResponseDTO>> getTransactionById() {
+        List<TransactionResponseDTO> transactions = transactionService.getMemberTransactions();
+        return ResponseEntity.ok(transactions);
+    }
+
 }
