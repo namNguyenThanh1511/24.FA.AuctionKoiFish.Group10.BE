28763b0d1d8a761f1a1eb29e25ef066a3b82dc54
khaqng <khanhquangnguyennqk@gmail.com>
2024-10-28 09:19:03 +0700

    fix api get list auctions request is accepted

--- src/main/java/com/group10/koiauction/service/AuctionRequestService.java
+++ src/main/java/com/group10/koiauction/service/AuctionRequestService.java
@@ -11,6 +11,7 @@ import com.group10.koiauction.exception.EntityNotFoundException;
 import com.group10.koiauction.model.request.ResponseAuctionRequestDTO;
 import com.group10.koiauction.model.request.AuctionRequestDTO;
 import com.group10.koiauction.model.request.AuctionRequestUpdateDTO;
+import com.group10.koiauction.model.response.AcceptedAuctionRequestResponse;
 import com.group10.koiauction.model.response.AuctionRequestResponse;
 import com.group10.koiauction.model.response.BreederResponseDTO;
 import com.group10.koiauction.repository.AccountRepository;
@@ -20,6 +21,9 @@ import com.group10.koiauction.utilities.AccountUtils;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
